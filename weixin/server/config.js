2f31ec7f1090b303448052e9e05b1789dce1b779
姜根生 <1240213281@qq.com>
2016-11-05 16:42:48 +0800

    上传文件

---
+++ weixin/server/config.js
@@ -0,0 +1,10 @@
+module.exports = function(){
+	return {
+		//AppID(应用ID)
+		appid: 'wxc8f88dfa1af81102',
+		//AppSecret(应用密钥)
+		appsecret: 'ba9bcaf2dc6550693fc889d4e100a3a2',
+		//Token(令牌)
+		token: 'niyang'
+	};
+};
